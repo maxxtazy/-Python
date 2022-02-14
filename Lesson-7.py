e7c0c32406ac85206a6671db9b6e550775477e0c
maxxtazy <md@sochi.com>
2022-02-15 00:18:13 +0300

    Домашнее задание к уроку 7

---
+++ Lesson-7.py
@@ -0,0 +1,111 @@
+# Задание 1
+class Matrix:
+    def __init__(self, list_1, list_2):
+        self.list_1 = list_1
+        self.list_2 = list_2
+
+    def __add__(self):
+        matr = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
+
+        for i in range(len(self.list_1)):
+
+            for j in range(len(self.list_2[i])):
+                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
+
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+
+    def __str__(self):
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+
+
+my_matrix = Matrix([[5, 18, 11],
+                    [6, 17, 23],
+                    [41, 50, 9]],
+                   [[45, 8, 2],
+                    [6, 7, 93],
+                    [24, 5, 97]])
+print(my_matrix.__add__())
+
+# Задание 2
+class Textile:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_s(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Общий расход ткани: \n'
+                   f' {(self.width / 6.5 + 0.5) + (self.height * 2 + 0.3)}')
+
+
+class Coat(Textile):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_c = round(self.width / 6.5 + 0.5)
+
+    def __str__(self):
+        return f'Расход ткани на пальто: {self.square_c}'
+
+
+class Suit(Textile):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_s = round(self.height * 2 + 0.3)
+
+    def __str__(self):
+        return f'Расход ткани на костюм: {self.square_s}'
+
+
+coat = Coat(3, 6)
+suit = Suit(2, 5)
+print(coat)
+print(suit)
+print(coat.get_sq_full)
+print(suit.get_sq_full)
+print(suit.get_square_c())
+print(suit.get_square_s())
+
+# Задание 3
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
+
+    def __str__(self):
+        return f'Результат операции {self.quantity * "*"}'
+
+    def __add__(self, other):
+        return Cell(self.quantity + other.quantity)
+
+    def __sub__(self, other):
+        return self.quantity - other.quantity if (self.quantity - other.quantity) > 0 else print('Отрицательно!')
+
+    def __mul__(self, other):
+        return Cell(int(self.quantity * other.quantity))
+
+    def __truediv__(self, other):
+        return Cell(round(self.quantity // other.quantity))
+
+    def make_order(self, cells_in_raw):
+        raw = ''
+        for i in range(int(self.quantity / cells_in_raw)):
+            raw += f'{"*" * cells_in_raw} \\n'
+        raw += f'{"*" * (self.quantity % cells_in_raw)}'
+        return raw
+
+
+cells1 = Cell(33)
+cells2 = Cell(9)
+print(cells1)
+print(cells1 + cells2)
+print(cells2 - cells1)
+print(cells2.make_order(5))
+print(cells1.make_order(10))
+print(cells1 / cells2)
